2ed596b3c6f4da0733caae17a0eac5a790966008
Jaqueline Alves <86419207+AS-Jaque@users.noreply.github.com>
2021-09-17 18:08:17 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+print ("Tabuada\n")
+contador = 1
+limite = 10
+tabuada = int(input("Gostaria de saber a tabuada de qual número: \n"))
+while(contador<=limite):
+    res = tabuada*contador
+    print(tabuada,"X",contador,"=",res,"\n")
+    contador += 1
+
+print("FIM DO PROGRAMA")
